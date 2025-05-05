d0ae7b6d5820c8374a0b28b531b2a7653eebf1d9
Walavalkar <Shreya.Walavalkar@ext.saint-gobain.com>
2025-05-05 14:54:51 +0530

    changed my main.tf azure configuration

--- main.tf
+++ main.tf
@@ -3,7 +3,11 @@ provider "azurerm" {
   subscription_id = "d39add2e-a062-4ac2-b217-b69b4fb14a19"
 }
 
+# resource "azurerm_resource_group" "example" {
+#   name     = "example-rg"
+#   location = "East US"
+# }
 resource "azurerm_resource_group" "example" {
-  name     = "example-rg"
+  name     = "example-rg1"
   location = "East US"
 }
